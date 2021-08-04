8192149189514b10d26eb905db3c6045dfe0d278
GianElli99 <gianelli99@hotmail.com>
2021-08-04 16:16:14 -0300

    contact form validations added and now it opens the default email client

--- js/contact.js
+++ js/contact.js
@@ -5,18 +5,28 @@ form.addEventListener('submit', HandleSubmit);
 
 function HandleSubmit(e) {
   e.preventDefault();
-  var nombre = document.getElementById('nombre').value;
-  console.log(nombre);
-  var email = document.getElementById('email').value;
-  var comentario = document.getElementById('comentario').value;
+  var userName = document.getElementById('user-name').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var comment = document.getElementById('comment').value.trim();
 
-  var data = {
-    nombre: nombre,
-    email: email,
-    comentario: comentario,
-  };
-  console.log(data);
-  SendDataToServer(serverUrl, data);
+  var validation = ValidateFields(userName, email, comment);
+  if (validation.isValid) {
+    var data = {
+      userName: userName,
+      email: email,
+      comment: comment,
+    };
+
+    SendDataToServer(serverUrl, data);
+    window.open(
+      'mailto:gianelli99@hotmail.com?subject=Comment about checkers, made by ' +
+        userName +
+        '&body=' +
+        comment
+    );
+  } else {
+    alert(validation.errorMsg);
+  }
 }
 
 function SendDataToServer(url, data) {
@@ -36,4 +46,31 @@ function SendDataToServer(url, data) {
     .catch(function (error) {
       console.log(error);
     });
+}
+function ValidateFields(name, email, comment) {
+  var isValid = true;
+  var errorMsg = '';
+
+  if (!isValidName(name)) {
+    isValid = false;
+    errorMsg += 'Invalid Name. ';
+  }
+  if (!isValidEmail(email)) {
+    isValid = false;
+    errorMsg += 'Invalid Email. ';
+  }
+  if (comment.trim().length < 5) {
+    isValid = false;
+    errorMsg += 'Invalid Comment.';
+  }
+
+  return { isValid: isValid, errorMsg: errorMsg };
+}
+function isValidEmail(email) {
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+function isValidName(name) {
+  var re = /^[a-z0-9]+$/i;
+  return re.test(name);
 }
